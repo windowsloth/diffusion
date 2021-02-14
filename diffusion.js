9407d814af414b25e97d5d11b8ccd2d4d61a9f40
windowsloth <67842472+windowsloth@users.noreply.github.com>
2021-02-14 15:08:59 -0500

    Update diffusion.js

--- diffusion.js
+++ diffusion.js
@@ -20,17 +20,18 @@ function setup() {
   pixelDensity(1);
   w = img.width;
   h = img.height;
-  createCanvas(w * 3, h);
+  createCanvas(w/* * 3*/, h);
   background(220);
   image(img, 0, 0);
 }
 
 function draw() {
   flowField();
+  diffuse();
 }
 
 function flowField() {
-  const step = .025;
+  const step = .01;
   let xoff = 0;
   for (let i = 0; i < w; i++) {
     let yoff = 0;
@@ -40,7 +41,7 @@ function flowField() {
       let noiseangle = map(noiseval, 0, 1, 0, TWO_PI);
       field.push(p5.Vector.fromAngle(noiseangle));
       stroke(noisecolor);
-      point(i + w * 2, j);
+      //point(i + w * 2, j);
       yoff += step;
     }
     xoff += step;
@@ -49,6 +50,19 @@ function flowField() {
 
 function diffuse() {
   loadPixels();
-
+  console.log(pixels.length);
+  console.log(field.length);
+  for (let i = 0; i < pixels.length / 4; i++) {
+    let xpos = i % width;
+    let ypos = floor(i / width);
+    xpos = xpos + field[i].x;
+    ypos = ypos + field[i].y;
+    let newval = round(xpos + width * ypos);
+    let pixelval = i * 4;
+    pixels[pixelval] = pixels[newval];
+    pixels[pixelval + 1] = pixels[newval + 1];
+    pixels[pixelval + 3] = pixels[newval + 2];
+    pixels[pixelval + 3] = pixels[newval + 3];
+  }
   updatePixels();
 }
